8d3e37b776d6be1b4c0e2ec7004057bf567780f9
Zarpor <gaspi.lami@gmail.com>
2019-05-18 18:30:08 +0200

    Added a class library
    Added class library convertor_berekenen to repository.

---
+++ Hex Dec And Binary Convertor/Convertor_berekenen.Lib/Berekenen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertor_berekenen.Lib
+{
+    public class Class1
+    {
+    }
+}
